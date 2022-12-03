ed3bc65b9c653cb60e6ab9008336c79bd40f5d45
Alex Albu <alexmhh1234@gmail.com>
2022-12-03 14:50:00 +0200

    [Add] Login implementation
    Finished implementing login design + added material-ui icons

--- src/containers/Login/Login.js
+++ src/containers/Login/Login.js
@@ -1,15 +1,17 @@
 import { useTranslation } from 'react-i18next';
 import axios from 'axios';
-import React from 'react';
+import React, { useState } from 'react';
+import VisibilityIcon from '@material-ui/icons/Visibility';
 
 import * as Styled from './Login.styled';
 import styles from 'Login.module.css';
 
 function Login() {
   const { t } = useTranslation();
+  const [passwordShown, setPasswordShown] = useState(false);
 
   async function loginRequest() {
-    axios({
+    await axios({
       method: 'post',
       url: '/user/12345', // TODO: Change
       data: {
@@ -19,24 +21,33 @@ function Login() {
     });
   }
 
+  function togglePassword() {
+    setPasswordShown(!passwordShown);
+  }
+
   return (
     <Styled.LoginContainer>
       <div className={styles.header}>
-        <h1>{t('Hello and welcome!')}</h1>
+        <h1>{t('Hello, Krafter!')}</h1>
       </div>
       <div className={styles.inputs}>
         <input type={'text'} className={styles.input} placeholder={'Username'} />
-        <input type={'password'} className={styles.input} placeholder={'Password'} />
+        <div className={styles.passwordContainer}>
+          <input type={passwordShown ? 'text' : 'password'} className={styles.input} placeholder={'Password'} />
+          <button onClick={togglePassword} style={{ marginLeft: '-4vw', background: 'none', border: 'none' }}>
+            <VisibilityIcon />
+          </button>
+        </div>
       </div>
       <div className={styles.footer}>
         <button className={styles.button} onClick={loginRequest}>
           <p>{t('Login')}</p>
         </button>
         <h4>
-          {t('Do not have an account?')} <a href={'https://facebook.com'}>{t('Register.')}</a>
+          {t('Do not have an account?')} <a href={'/register'}>{t('Register.')}</a>
         </h4>
         <h4>
-          {t('Forgot your password?')} <a href={'https://facebook.com'}>{t('Reset.')}</a>
+          {t('Forgot your password?')} <a href={'https://facebook.com'}>{t('Reset it.')}</a>
         </h4>
       </div>
     </Styled.LoginContainer>
