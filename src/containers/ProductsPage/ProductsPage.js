f9bb9b67462872600ec4e0c8294933989b754ec7
Vlad Stefan <vladstefan.cira@gmail.com>
2022-12-18 13:46:02 +0200

    implemented

--- src/containers/ProductsPage/ProductsPage.js
+++ src/containers/ProductsPage/ProductsPage.js
@@ -10,13 +10,13 @@ function ProductsPage() {
 
   useEffect(function(){
     axios
-    .get("https://run.mocky.io/v3/8bdacd9d-1876-4f2e-a303-c0a829699825") //TODO change to actual URL
+    .get("https://run.mocky.io/v3/a1f2561e-a8cb-4e91-9ed8-7ed7f6f950e4") //TODO change to actual URL
     .then(res => setProducts(res.data))
     .then(error => console.log(error))
   }, [])
 
   return (
-    <Flex height="100%" width="100vw" justifyContent={'left'} flexWrap="wrap" flexDirection="row" alignItems={'center'}>
+    <Flex height="100%" width="100vw" justifyContent={'space-around'} flexWrap="wrap" flexDirection="row" alignItems={'center'}>
       { products.map((product) =>  (
         <div key={product.id} style={{margin: '4.5em', height: '369px', width: '300px'}} >
           <ProductsCard key={product.id} {...product}>
